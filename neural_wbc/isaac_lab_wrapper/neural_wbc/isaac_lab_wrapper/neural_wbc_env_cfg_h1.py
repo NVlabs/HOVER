02c8249bf1a7f34d3f3ac10138b0c5b5330a883d
xuhao3e8 <xuhao3e8@buaa.edu.cn>
2025-07-30 11:22:11 +0800

    Fix failed problem if do not have stable_punch.pkl

--- neural_wbc/isaac_lab_wrapper/neural_wbc/isaac_lab_wrapper/neural_wbc_env_cfg_h1.py
+++ neural_wbc/isaac_lab_wrapper/neural_wbc/isaac_lab_wrapper/neural_wbc_env_cfg_h1.py
@@ -285,7 +285,7 @@ class NeuralWBCEnvCfgH1(NeuralWBCEnvCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.reference_motion_manager.motion_path = get_data_path("motions/stable_punch.pkl")
+        self.reference_motion_manager.motion_path = ""
         self.reference_motion_manager.skeleton_path = get_data_path("motion_lib/h1.xml")
 
         if self.terrain.terrain_generator == HARD_ROUGH_TERRAINS_CFG:
